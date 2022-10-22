4cec56b104b89a40921604f3426bfdd7a925a3f1
AyakaOoshima <och1215bi3@gmail.com>
2022-10-22 20:32:48 +0900

    言葉　新規登録機能作成

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -6,4 +6,14 @@ class WordsController < ApplicationController
    def new
       @words = Word.new
    end
+
+   def create
+      Word.create(word_params) 
+      redirect_to root_path 
+   end
+
+   private
+   def word_params
+      params.require(:Word).permit(:name, :word, :text, :image)
+   end
 end
