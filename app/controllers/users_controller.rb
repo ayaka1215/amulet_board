c1cc5bd593efedf4afe6bd2579bbc934b0e7de62
AyakaOoshima <och1215bi3@gmail.com>
2022-10-26 00:43:30 +0900

    マイページ　お守り言葉を降順に変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
     def show
         user = User.find(params[:id])
         @nickname = user.nickname
-        @words = current_user.words
+        @words = current_user.words.order("created_at DESC")
     end
 end
